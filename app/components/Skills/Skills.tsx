f52651c8b2102f87d3c54d27637884f108d6e3e7
mathieubourkel <mathieu.bourkel@gmail.com>
2024-03-28 17:46:57 +0100

    modif css

--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -3,7 +3,7 @@ import CardSkill from "./CardSkill";
 import "./skills.css"
 export default function Skills() {
     return (
-      <div className='container flex 3xl:flex-nowrap flex-wrap gap-5 justify-center py-10 2xl:py-0'>
+      <div className='container flex 3xl:flex-nowrap flex-wrap gap-5 justify-center py-10'>
           {skills.map((cat, index) => (
             <CardSkill  key={index} title={skillsCat[index].title} icon={skillsCat[index].icon} skills={cat}/>
           ))}
